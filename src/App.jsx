6f78de3c1444386c1701a806db93bf1be93244b5
Zachary-Collier <zachary.collier@saintleo.edu>
2023-08-08 19:13:27 -0400

    Added code to this file

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const primaryBaseUrl = 'https://strangers-things.herokuapp.com/api/'
+//construct and save your COHORT_NAME in a variable
+const  cohortName = '2209-FTB-ET-WEB-FT' 
+//construct and save your BASE_URL in a variable
+const baseUrl = primaryBaseUrl + cohortName
 
 function App() {
-  const [count, setCount] = useState(0)
+// functionality code goes in here //
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   return <> {/* your code in here */}</>
 }
-
 export default App
