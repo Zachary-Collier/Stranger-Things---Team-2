66ff0a6b1030be22c3ef58c25b51ecf750a4b998
Zachary-Collier <zachary.collier@saintleo.edu>
2023-08-14 19:11:26 -0400

    Lots of changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Posts from "./Pages/Posts";
 import { useState } from "react";
 import "./App.css";
-import Posts from "./Pages/Posts";
 
 function App() {
- 
-  return <>
-  <Posts />
-  </>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/profileCreated.jsx
@@ -0,0 +1,7 @@
+export default function profileCreated() {
+    return (
+      <div>
+        <h1>Register</h1>
+      </div>
+    );
+  }
